b12213694253748abf6a0636e175b69f6e337258
liuzewei1990 <543347844@qq.com>
2017-02-14 22:38:23 +0800

    标题
    微信H5开发

---
+++ item/scripts/Joy.js
@@ -0,0 +1,369 @@
+﻿
+;; void (function (win, doc) {
+
+	var UA = navigator.userAgent,
+	    isAndroid = /android|adr/gi.test(UA),
+	    isIos = /iphone|ipod|ipad/gi.test(UA) && !isAndroid, // 据说某些国产机的UA会同时包含 android iphone 字符
+	    isMobile = isAndroid || isIos;  // 粗略的判断
+
+	win.UAS = {
+		isAndroid: isAndroid,
+		isIos: isIos
+	}
+
+	win.mobileUtil = {
+		isAndroid: isAndroid,
+		isIos: isIos,
+		isMobile: isMobile,
+
+		isNewsApp: /NewsApp\/[\d\.]+/gi.test(UA),
+		isWeixin: /MicroMessenger/gi.test(UA),
+		isQQ: /QQ\/\d/gi.test(UA),
+		isYixin: /YiXin/gi.test(UA),
+		isWeibo: /Weibo/gi.test(UA),
+		isTXWeibo: /T(?:X|encent)MicroBlog/gi.test(UA),
+
+		tapEvent: isMobile ? 'tap' : 'click',
+
+		fixScreen: function () {
+			/// <summary>
+			/// 页面适配
+			/// </summary>
+			var metaEl = doc.querySelector('meta[name="viewport"]'),
+			    metaCtt = metaEl ? metaEl.content : '',
+			    matchScale = metaCtt.match(/initial\-scale=([\d\.]+)/),
+			    matchWidth = metaCtt.match(/width=([^,\s]+)/);
+
+			if (!metaEl) { // REM
+				var docEl = doc.documentElement,
+				    maxwidth = docEl.dataset.mw || 750, // 每 dpr 最大页面宽度
+				    dpr = isIos ? Math.min(win.devicePixelRatio, 3) : 1,
+				    scale = 1 / dpr,
+				    tid;
+
+				docEl.removeAttribute('data-mw');
+				docEl.dataset.dpr = dpr;
+				metaEl = doc.createElement('meta');
+				metaEl.name = 'viewport';
+				metaEl.content = fillScale(scale);
+				docEl.firstElementChild.appendChild(metaEl);
+
+				var refreshRem = function () {
+					var width = docEl.getBoundingClientRect().width;
+					if (width / dpr > maxwidth) {
+						width = maxwidth * dpr;
+					}
+					var rem = width / 16;
+					docEl.style.fontSize = rem + 'px';
+				};
+
+				win.addEventListener('resize', function () {
+					clearTimeout(tid);
+					tid = setTimeout(refreshRem, 300);
+				}, false)
+				win.addEventListener('pageshow', function (e) {
+					if (e.persisted) {
+						clearTimeout(tid);
+						tid = setTimeout(refreshRem, 300);
+					}
+				}, false);
+
+				refreshRem();
+			}
+			else if (isMobile && !matchScale && (matchWidth && matchWidth[1] != 'device-width')) { // 定宽
+				var width = parseInt(matchWidth[1]),
+				    iw = win.innerWidth || width,
+				    ow = win.outerWidth || iw,
+				    sw = win.screen.width || iw,
+				    saw = win.screen.availWidth || iw,
+				    ih = win.innerHeight || width,
+				    oh = win.outerHeight || ih,
+				    ish = win.screen.height || ih,
+				    sah = win.screen.availHeight || ih,
+				    w = Math.min(iw, ow, sw, saw, ih, oh, ish, sah),
+				    scale = w / width;
+				if (scale < 1) {
+					metaEl.content = metaCtt + ',' + fillScale(scale);
+				}
+			}
+
+			function fillScale(scale) {
+				return 'initial-scale=' + scale + ',maximum-scale=' + scale + ',minimum-scale=' + scale;
+			}
+		}(),
+
+		/**
+		 * 转href参数成键值对
+		 * @param href {string} 指定的href，默认为当前页href
+		 * @returns {object} 键值对
+		 */
+		getSearch: function (href) {
+			href = href || win.location.search;
+			var data = {}, reg = new RegExp("([^?=&]+)(=([^&]*))?", "g");
+			href && href.replace(reg, function ($0, $1, $2, $3) {
+				data[$1] = $3;
+			});
+			return data;
+		}
+	};
+
+	function MyAudio(obj) {
+		/// <summary>
+		/// AudioContext音乐构造函数
+		/// </summary>
+		/// <param name="obj" type="type"></param>
+		if (!obj) return;
+		this.context = new (window.AudioContext || window.webAudioContext || window.webkitAudioContext)();
+		this.audioBuffer = null;
+		this.source = null;
+		this.loop = obj.isLoop;
+		var _this = this;
+		//下载音乐文件
+		var xhr = new XMLHttpRequest();
+		xhr.open('GET', obj.src, true);
+		xhr.responseType = 'arraybuffer';
+		xhr.onload = function (e) {
+			initSound(this.response);
+			//下载完成解码操作
+			function initSound(arrayBuffer) {
+				_this.context.decodeAudioData(arrayBuffer, function (buffer) {
+					_this.audioBuffer = buffer;
+				}, function (e) {
+					console.log('Error decoding file', e);
+				});
+			}
+		};
+		xhr.send();
+	};
+
+	MyAudio.prototype.play = function () {
+		/// <summary>
+		/// 音乐播放方法
+		/// </summary>
+		if (this.audioBuffer) {
+			this.source = this.context.createBufferSource();
+			this.source.buffer = this.audioBuffer;
+			this.source.loop = this.loop;
+			this.source.connect(this.context.destination);
+			this.source.start(0);
+		}
+	};
+
+	MyAudio.prototype.stop = function () {
+		/// <summary>
+		/// 音乐停止方法
+		/// </summary>
+		if (this.source) {
+			this.source.stop(0);
+		}
+	};
+	//检测如果是IOS系统就使用AudioContext音乐对象
+	if (isIos) win.musics = {
+		mail: new MyAudio({
+			src: "audio/mail.mp3"
+		}),
+		blued: new MyAudio({
+			src: "audio/blued.mp3"
+		}),
+		unlock: new MyAudio({
+			src: "audio/unlock.mp3"
+		}),
+		tockLoop: new MyAudio({
+			src: "audio/tock-loop.mp3",
+			isLoop: true
+		}),
+		tink: new MyAudio({
+			src: "audio/tink.mp3"
+		})
+	};
+
+	//调用播放方法
+	win.playMusic = function (id) {
+		if (isIos) {
+			musics[id].play(); //ios 下播放AudioContext
+		} else if (isAndroid) {
+			doc.getElementById(id).play(); //android 下播放Audio
+		} else {
+			doc.getElementById(id).play(); //其他 播放Audio
+		}
+	};
+
+	win.F = {
+		enter: function () {
+			//如果有本地储存数据 或者 url中有数据就进入结果页面
+		},
+		domain: function () {
+			/// <summary>
+			/// 主域名
+			/// </summary>
+			/// <returns type=""></returns>
+			return (!(/\?/.test(location.href)) ? location.href : /([^?]+)\??/.exec(location.href)[1]);
+		}(),
+		is_object: function (obj) {
+			/// <summary>
+			/// 检测一个对象是否为空
+			/// </summary>
+			/// <param name="obj" type="type">要检测的对象</param>
+			/// <returns type=""></returns>
+			if (!obj) return;
+			if (Object.keys(obj).length === 0) return true;
+			else return false;
+		},
+		is_weixn: function () {
+			/// <summary>
+			/// 检测是否是微信浏览器打开
+			/// </summary>
+			/// <returns type=""></returns>
+			var ua = navigator.userAgent.toLowerCase();
+			if (ua.match(/MicroMessenger/i) == "micromessenger") {
+				return true;
+			} else {
+				return false;
+			}
+		},
+		is_value_type: function () {
+			/// <summary>
+			/// 检测一个值的具体类型
+			/// </summary>
+			/// <returns type=""></returns>
+			return this.toString.call(val).slice(8, this.toString.call(val).length - 1);
+		},
+		get_query_string: function () {
+			/// <summary>
+			/// 获取url参数
+			/// </summary>
+			/// <returns type=""></returns>
+			var obj = {};
+			var result = location.search.match(new RegExp("[\?\&][^\?\&]+=[^\?\&]+", "g"));
+			if (!result) return obj;
+			for (var i = 0; i < result.length; i++) {
+				result[i] = result[i].substring(1).split("=");
+				var key = decodeURIComponent(result[i][0]),
+				        value = decodeURIComponent(result[i][1]);
+				var num = Number(value);
+				obj[key] = isNaN(num) ? value : num;
+			}
+			return Object.freeze(obj);
+		}(),
+		get_window_view: function () {
+			/// <summary>
+			/// 返回window宽和高
+			/// </summary>
+			/// <returns type=""></returns>
+			var obj = {};
+			obj.w = window.innerWidth;
+			obj.h = window.innerHeight;
+			return obj;
+		},
+		get_localStorage: function () {
+			/// <summar>y
+			/// 获取本地储存数据
+			/// </summary>
+			/// <returns type=""></returns>
+			if (!localStorage) return;
+			var obj = {};
+			for (var i in localStorage) {
+				obj[i] = localStorage[i];
+			}
+			return obj;
+		}(),
+		set_accredit: function (bool) {
+			/// <summary>
+			/// 开启微信授权
+			/// </summary>
+			/// <param name="bool" type="type">是否获取用户信息</param>
+			if (this.get_query_string["openid"]) return;
+			window["location"]["replace"]("http://weixin.huosu.com/api/index.php?act=authorize&url=" + window["location"]["href"] + "&is_userinfo=" + (bool ? "1" : "0"));
+		},
+		set_share: function (obj1, obj2, callBack) {
+			/// <summary>
+			/// 微信自定义分享
+			/// </summary>
+			/// <param name="obj" type="type">分享好友参数</param>
+			/// <param name="obj2" type="type">分享朋友圈参数</param>
+			/// <param name="callBack" type="type">分享成功后的回调函数</param>
+			var imgUrl = "http://yunfan.huosu.com/customer/boss/icon.jpg"; //上线后改地址
+			$.ajax({
+				type: "GET", async: true, dataType: "jsonp", url: "http://weixin.huosu.com/api/index.php", data: { act: "sign", }, success: function (obj, info) { wxConfig(obj) },
+				error: function () { }
+			});
+			function wxConfig(obj) {
+				wx.config({
+					debug: false, appId: obj.data.appId, timestamp: obj.data.timestamp, nonceStr: obj.data.nonceStr, signature: obj.data.signature,
+					jsApiList: ['checkJsApi', 'onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareWeibo', 'onMenuShareQZone', 'hideMenuItems', 'showMenuItems', 'hideAllNonBaseMenuItem', 'showAllNonBaseMenuItem', 'translateVoice', 'startRecord', 'stopRecord', 'onVoiceRecordEnd', 'playVoice', 'onVoicePlayEnd', 'pauseVoice', 'stopVoice', 'uploadVoice', 'downloadVoice', 'chooseImage', 'previewImage', 'uploadImage', 'downloadImage', 'getNetworkType', 'openLocation', 'getLocation', 'hideOptionMenu', 'showOptionMenu', 'closeWindow', 'scanQRCode', 'chooseWXPay', 'openProductSpecificView', 'addCard', 'chooseCard', 'openCard']
+				});
+				wx.ready(function () {
+					wx.onMenuShareAppMessage({
+						title: obj1.title, desc: obj1.desc, link: obj1.link, imgUrl: imgUrl,
+						trigger: function (res) { },
+						success: function (res) { callBack && callBack() },
+						cancel: function (res) { },
+						fail: function (res) { }
+					});
+					wx.onMenuShareTimeline({
+						title: obj2.title, link: obj2.link, imgUrl: imgUrl,
+						trigger: function (res) { },
+						success: function (res) { callBack && callBack() },
+						cancel: function (res) { },
+						fail: function (res) { }
+					});
+
+				});
+			};
+		},
+		set_ajax: function (data, callback) {
+			/// <summary>
+			/// 请求Ajax
+			/// </summary>
+			/// <param name="data" type="type">请求参数</param>
+			/// <param name="callback" type="type">回调函数</param>
+			$.ajax({
+				type: "GET",
+				async: false,
+				dataType: "jsonp",
+				url: "http://mobi.cntosee.com/qiandao/",
+				data: data,
+				success: function (obj) { if (obj instanceof Object) { callback(obj); } else { console.info("Ajax返回结果类型错误") } },
+				error: function () { console.info("Ajax链接失败") }
+			});
+		},
+		set_ajax_yunfan: function (data, callback) {
+			/// <summary>
+			/// 请求Ajax
+			/// </summary>
+			/// <param name="data" type="type">请求参数</param>
+			/// <param name="callback" type="type">回调函数</param>
+			$.ajax({
+				type: "GET",
+				async: false,
+				dataType: "jsonp",
+				url: "http://yunfan.huosu.com/customer/boss/api/index.php",
+				data: data,
+				success: function (obj) { if (obj instanceof Object) { callback(obj); } else { console.info("Ajax返回结果类型错误") } },
+				error: function () { console.info("Ajax链接失败") }
+			});
+		},
+		set_localStorage: function (obj) {
+			/// <summary>
+			/// 设置本地储存
+			/// </summary>
+			/// <param name="obj" type="type">要设置的参数对象</param>
+			if (!obj) return;
+			for (var i in obj) localStorage[i] = obj[i];
+		},
+		remove_localStorage: function () {
+			/// <summary>
+			/// 删除本地储存
+			/// </summary>
+			localStorage.removeItem("wendao_id");
+			localStorage.removeItem("wendao_title");
+			localStorage.removeItem("wendao_desc");
+		},
+
+	};
+
+
+})(window, document);
+
+
+
---
+++ item/scripts/index.js
@@ -0,0 +1,468 @@
+﻿/// <reference path="Joy.js" />
+document.body.addEventListener('touchmove', bodyPreventDefault, false);
+function bodyPreventDefault(event) {
+	event.preventDefault();
+}
+
+var imgUrl = "images/boss.jpg";
+
+//获取用户信息
+var WX_USER = {
+	openid: F.get_query_string["openid"],
+	name: F.get_query_string["nickname"],
+	headerUrl: F.get_query_string["headimgurl"] || imgUrl
+};
+
+console.log(WX_USER);
+F.set_share({
+	title: "一封来自Boss直聘的邀请函",
+	desc: "其他999个Boss都去了......",
+	link: F.domain
+}, {
+	title: "一封来自Boss直聘的邀请函",
+	link: F.domain
+}, function () {
+	F.set_ajax_yunfan({ act: "share" }, function () { });
+});
+var isOverflow = true;
+var s = "cubic-bezier(0,.78,.32,1.08)";
+var ajaxDataBase64 = "";
+var GlabalHeaderUrl = "";
+function fadeInP2(index, callBack) {
+	/// <summary>
+	/// 聊天窗口消息调用
+	/// </summary>
+	/// <param name="index" type="type">图片编号</param>
+	/// <param name="callBack" type="type">发送成功的回调</param>
+	var p2 = $(".page2");
+	var img = new Image();
+	img.onload = function () {
+		//填充元素并返回元素集合
+		var oLis = p2.find($(".p2_newsList").append('<li><img src="' + this.src + '" alt="Alternate Text" /></li>').find("li"));
+
+		oLis.eq(oLis.size() - 1).css({ height: "0px" }).show(1, function () {
+			//撑开消息内容
+			playMusic("mail");
+			var oImgHeight = $(this).find("img").height();
+			$(this).animate({ height: oImgHeight + "px" }, 300, function () {
+				callBack();
+			});
+			//如果消息高度溢出了使新的消息向上顶
+			var top = $(this).position().top + oImgHeight,
+			 inputHeight = p2.find(".input").height(),
+			 maxHeight = p2.height() - inputHeight;
+			if (top > maxHeight && isOverflow) {
+				console.log("超了");
+				$(".newsContainer").css({ bottom: inputHeight + "px" });
+				$(".p2_newsList").css({ bottom: "0%" });
+				isOverflow = false;
+			}
+		})
+	};
+	img.src = "images/p2_news_" + index + ".png";
+}
+
+function fadeInP1(index) {
+	/// <summary>
+	/// 首页消息调用方法
+	/// </summary>
+	/// <param name="index" type="type">图片编号</param>
+	var img = new Image();
+	img.onload = function () {
+		$($(".p1_newsList").prepend('<li><img src="' + this.src + '" alt="Alternate Text" /></li>').find("li")[0]).css({ height: "0px" }).show(1, function () {
+			playMusic("mail");
+			var height = $(this).find("img").height();
+			$(this).animate({ height: height + "px" }, 300);
+		})
+	};
+	img.src = "images/news_" + index + ".png";
+}
+
+function timer(time) {
+	/// <summary>
+	/// //Promise状态控制
+	/// </summary>
+	/// <param name="time" type="type">延迟加载的时间</param>
+	return new Promise(function (resolve, reject) {
+		setTimeout(resolve, time);
+	});
+}
+
+function drawCanvas(headerImgUrl, nickName, callBack) {
+	var oC = document.createElement("canvas");
+	var scale = 0.8;
+	oC.width = 750 * scale;
+	oC.height = 1334 * scale;
+	var cTx = oC.getContext("2d");
+
+	var background = new Image();
+	var headerImg = new Image();
+	background.onload = headerImg.onload = function () {
+		this.isLoaded = true;
+		if (background.isLoaded && headerImg.isLoaded) {
+			var y = 475 * scale, r = 80 * scale;
+			//绘制头像
+			cTx.fillStyle = "#000";
+			cTx.arc(oC.width / 2, y, r - 2, 0, Math.PI / 180 * 360, true);
+			cTx.fill();
+			cTx.globalCompositeOperation = "source-atop";
+			cTx.drawImage(headerImg, oC.width / 2 - r, y - r, r * 2, r * 2);
+			cTx.globalCompositeOperation = "destination-over";
+			cTx.drawImage(background, 0, 0, oC.width, oC.height);
+			//绘制文字
+			cTx.globalCompositeOperation = "source-atop";
+			cTx.font = "28px 微软雅黑";
+			cTx.fillStyle = "#000";
+			cTx.textAlign = "center";
+			cTx.textBaseline = "middle";
+			cTx.fillText(nickName, oC.width / 2, y + r + 60 * scale);
+			var resultBase64 = oC.toDataURL();
+			//回调
+			setTimeout(function () {
+				callBack(resultBase64);
+			}, 500);
+		}
+	};
+	background.src = "images/p5Bg_" + Math.floor(Math.random() * 3 + 1) + ".jpg";
+	headerImg.src = headerImgUrl;
+}
+
+function titleSet(titleStr) {
+	/// <summary>
+	/// title重新设置
+	/// </summary>
+	/// <param name="title" type="type">新title</param>
+	if (UAS.isIos) {
+		var $body = $("body");
+		document.title = titleStr;
+		var $iframe = $('<iframe src="/favicon.ico"></iframe>');
+		$iframe.on('load', function () {
+			setTimeout(function () {
+				$iframe.off('load').remove();
+			}, 0);
+		}).appendTo($body);
+	} else {
+		document.title = titleStr;
+	}
+}
+
+function fadeInResultPage(url) {
+	var oImg = new Image();
+	oImg.onload = function () {
+		$(".pace").remove();
+		$(".page4").remove();
+		$(".page5").append('<img src="' + this.src + '" alt="" /><p>长按保存图片</p>').fadeIn(500);
+	}
+	oImg.src = url;
+}
+
+//加载器
+Pace.options = {
+	ajax: false,
+	document: false,
+	eventLag: false,
+};
+Pace.on('done', function () {
+	$(".pace").fadeOut(500);
+
+	//首屏开始
+	timer(500)
+	    .then(function () {
+	    	F.set_ajax_yunfan({ act: "uv", openid: WX_USER.openid }, function () { });
+	    	F.set_ajax_yunfan({ act: "pv" }, function () { });
+	    	$(".page1").fadeIn(500); return timer(1000);
+	    })
+	//发送消息
+	    .then(function () { fadeInP1(1); return timer(1000) })
+	    .then(function () { fadeInP1(2); return timer(800) })
+	    .then(function () { fadeInP1(3); return timer(800) })
+	    .then(function () { fadeInP1(4); return timer(800) })
+	    .then(function () { fadeInP1(5); return timer(0) })
+	    .then(function () {
+	    	return new Promise(function (resolve, reject) {
+	    		$(".page1").on("touchend", function () {
+	    			$(this).hide(1, function () {
+	    				resolve();
+	    			});
+	    			playMusic("unlock");
+	    		})
+	    	});
+	    })
+	 //进入启动页面
+	    .then(function () { $(".page1").remove(); $(".start-up").show(); return timer(1000) })
+	 //进入聊天窗口
+	    .then(function () {
+	    	$(".page2").show();
+	    	//设置title页面标题
+	    	titleSet("Boss直聘融资发布会（10）");
+	    	return new Promise(function (resolve) {
+	    		$(".start-up").fadeOut(500, function () {
+	    			$(this).remove();
+	    			setTimeout(resolve, 1000);
+	    		});
+	    	});
+	    })
+
+	 //发送聊天消息
+	    .then(function () { return new Promise(function (resolve) { fadeInP2(1, function () { setTimeout(resolve, 800) }) }) })
+	    .then(function () { return new Promise(function (resolve) { fadeInP2(2, function () { setTimeout(resolve, 900) }) }) })
+	    .then(function () { return new Promise(function (resolve) { fadeInP2(3, function () { setTimeout(resolve, 800) }) }) })
+	    .then(function () { return new Promise(function (resolve) { fadeInP2(4, function () { setTimeout(resolve, 900) }) }) })
+	    .then(function () { return new Promise(function (resolve) { fadeInP2(5, function () { setTimeout(resolve, 800) }) }) })
+	    .then(function () { return new Promise(function (resolve) { fadeInP2(6, function () { setTimeout(resolve, 900) }) }) })
+	    .then(function () { return new Promise(function (resolve) { fadeInP2(7, function () { setTimeout(resolve, 800) }) }) })
+	    .then(function () { return new Promise(function (resolve) { fadeInP2(8, function () { setTimeout(resolve, 900) }) }) })
+	    .then(function () { return new Promise(function (resolve) { fadeInP2(9, function () { setTimeout(resolve, 800) }) }) })
+	    .then(function () { return new Promise(function (resolve) { fadeInP2(10, function () { setTimeout(resolve, 900) }) }) })
+	    .then(function () {
+	    	//最后一条对话消息处理
+	    	return new Promise(function (resolve) {
+	    		var oImg = new Image();
+	    		oImg.onload = function () {
+	    			playMusic("mail");
+	    			$(".p2_newsList").append($("#boss").html())
+				    .find(".wx_name").html(WX_USER.name).end()
+				    .find(".header").html('<img src="' + this.src + '" alt="Alternate Text" />')
+	    			setTimeout(resolve, 2000)
+	    		};
+	    		oImg.src = "images/boss.jpg";
+	    	})
+	    })
+	 //弹出虚拟键盘
+	    .then(function () {
+	    	return new Promise(function (resolve) {
+
+	    		$(".inputContainer").find(".send").show(1).end().find(".hidden").animate({ height: $(".key").height() + "px" }, function () {
+	    			$(".newsContainer").animate({ bottom: ($(".key").height() + $(this).prev().height()) + "px" }, 200);
+	    			$(this).find(".key").addClass("keyAnimate");
+	    			playMusic("tockLoop");
+	    			//打字机效果
+	    			var input = $(this).prev();
+	    			input.html("");
+	    			var txt = "哈哈，给我留个前排座位。";
+	    			var str = "";
+	    			for (var i = 0; i < txt.length; i++) {
+	    				(function (i) {
+	    					setTimeout(function () {
+	    						str += txt[i];
+	    						input.html("").html(str);
+	    					}, 200 * i + 500);
+	    				})(i);
+	    			}
+	    			//文字打完后停止动画
+	    			setTimeout(function () {
+	    				$(".hidden").find(".key").css({ backgroundPositionY: "0%" }).removeClass("keyAnimate");
+	    				if (UAS.isIos) musics["tockLoop"].stop();
+	    				resolve();
+	    			}, 2900);
+	    		});
+	    	});
+	    })
+	 //显示蒙层 提示用户点击发送按钮
+	    .then(function () {
+	    	return new Promise(function (resolve) {
+	    		$(".shadow").show().find(".mask").on("touchend", function () {
+	    			playMusic("tink");
+	    			resolve();
+	    		})
+	    	});
+	    })
+	 //显示用户发送的消息
+	    .then(function () {
+	    	$(".shadow").hide();
+	    	$(".input").text("");
+	    	return new Promise(function (resolve) {
+	    		var oImg = new Image();
+	    		oImg.onload = function () {
+	    			playMusic("mail");
+	    			$(".p2_newsList").append($("#self").html())
+				    .find(".self").find("span").html(WX_USER.name).end()
+				    .find(".header").html('<img src="' + this.src + '" alt="Alternate Text" />');
+	    			setTimeout(resolve, 2000);//2秒后进入介绍页
+	    		};
+	    		oImg.src = WX_USER.headerUrl;
+	    	});
+	    })
+	//进入介绍页
+	    .then(function () {
+	    	$(".page2").remove();
+	    	titleSet("Boss直聘融资发布会");
+	    	//预加载P3的动画帧
+	    	var oImg = new Image();
+	    	oImg.onload = function () {
+	    		$(".page3").show();
+	    	};
+	    	oImg.src = "images/mdds.png";
+
+	    	return new Promise(function (resolve) {
+	    		$(".page3").find(".mdds").on("webkitAnimationEnd", function () {
+	    			setTimeout(resolve, 500)
+	    		})
+	    	});
+	    })
+	    .then(function () { $(".mdds").css({ top: "3%", opacity: 1 }); return timer(200); })
+	    .then(function () { $(".p3_1").css({ top: "17%", opacity: 1 }); return timer(300); })
+	    .then(function () { $(".p3_2").css({ top: "25%", opacity: 1 }); return timer(200); })
+	    .then(function () { $(".p3_3").css({ top: "31%", opacity: 1 }); return timer(500); })
+	    .then(function () {
+	    	//解绑 Body 的默认行为
+	    	document.body.removeEventListener('touchmove', bodyPreventDefault, false);
+	    	$(".p3_4").css({ top: "36%", opacity: 1 });
+	    	return new Promise(function (resolve) {
+	    		//点击报名
+	    		$(".signBtn").on("click", function () {
+	    			$(".pace").css({ zIndex: "-10000" });
+	    			playMusic("tink");
+	    			//检测是否已经报过名
+	    			$.ajax({
+	    				type: "GET",
+	    				async: false,
+	    				dataType: "jsonp",
+	    				url: "http://yunfan.huosu.com/customer/boss/api/index.php",
+	    				data: {
+	    					act: "getpic",
+	    					openid: WX_USER.openid
+	    				},
+	    				success: function (obj) {
+	    					if (obj instanceof Object) {
+	    						if (obj.code == 200) {
+	    							var url = obj.data.data;
+	    							$(".page3").remove();
+	    							fadeInResultPage(url);
+	    						}
+	    						if (obj.code == 300) {
+	    							resolve();
+	    						}
+
+	    					} else { console.info("Ajax返回结果类型错误") }
+	    				},
+	    				error: function () { console.info("Ajax链接失败") }
+	    			});
+
+	    		});
+	    	});
+	    })
+	.then(function () {
+		$(".page3").remove();
+		$("body").scrollTop(0);
+
+		var p4 = $(".page4");
+		var currEle = null;
+		p4.fadeIn(500);
+		var inputs = p4.find("input[type=text]").each(function (i, ele) {
+			$(ele).on("focus", function () {
+				if (currEle) currEle.removeClass("checkend");
+				$(this).val("").css({ color: "#FFF" }).addClass("checkend");
+				currEle = $(this);
+			});
+		});
+		var u_name = $("#u_name"), g_name = $("#g_name"), u_zhiwei = $("#u_zhiwei"), u_eamil = $("#u_eamil"), u_phone = $("#u_phone");
+		var formData = {};
+		p4.fadeIn(800, function () {
+
+			//提前处理微信头像保存本地服务器
+			$.ajax({
+				type: "GET",
+				async: false,
+				dataType: "jsonp",
+				url: "http://yunfan.huosu.com/customer/boss/api/index.php",
+				data: {
+					act: "photo",
+					photo: decodeURIComponent(WX_USER.headerUrl)
+				},
+				success: function (obj) {
+					if (obj instanceof Object) {
+						if (obj.code == 200) GlabalHeaderUrl = obj.data;
+						else GlabalHeaderUrl = "images/wx.png";
+					} else { console.info("Ajax返回结果类型错误") }
+				},
+				error: function () { console.info("Ajax链接失败") }
+			});
+
+		});
+
+		return new Promise(function (resolve) {
+			p4.find("#btn").on("click", function () {
+				playMusic("tink");
+				//遍历检查哪个文本没有填写
+				for (var i = 0; i < inputs.size() ; i++) {
+					var currInput = inputs.eq(i);
+					if (currInput.val() === "" || currInput.val() === currInput.attr("data-info")) {
+						currInput.val(currInput.attr("data-info")).css({ color: "red" });
+						return;
+					}
+				}
+				////验证邮箱
+				if (!/^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/.test(u_eamil.val())) {
+					u_eamil.val("邮箱地址填写错误").css({ color: "red" });
+					return;
+				}
+				//验证手机号
+				if (!(/^1[3|4|5|7|8]\d{9}$/.test(u_phone.val()))) {
+					u_phone.val("手机号格式不正确").css({ color: "red" });
+					return;
+				}
+				//存入对象
+				inputs.each(function (i, ele) {
+					formData[ele.id] = $(ele).val();
+				});
+				//显示处理中...
+				$(".pace").css({ backgroundColor: "rgba(0,0,0,0.7)", top: $("body").scrollTop() + "px" }).css({ zIndex: "1000" }).show(1, function () {
+					$(this).find(".pace-progress").removeAttr("data-progress-text").html("请稍等...");
+					document.body.addEventListener("touchmove", bodyPreventDefault, false);
+				});
+				//防止GIF图卡顿添加一个延迟执行下一步
+				setTimeout(function () {
+					resolve(formData);
+				}, 500);
+			});
+		});
+	})
+	.then(function (formData) {
+		//如果过来的数据存在的话
+		if (!F.is_object(formData)) {
+
+			return new Promise(function (resolve) {
+				//生成Canvas画布
+				drawCanvas(GlabalHeaderUrl, formData.u_name + " • " + formData.g_name, function (resultBase64) {
+					//将生成后的base64保存到后台
+					$.ajax({
+						type: "POST",
+						async: false,
+						dataType: "json",
+						url: "http://yunfan.huosu.com/customer/boss/api/index.php",
+						data: {
+							act: "pic",
+							openid: WX_USER.openid,
+							nickname: WX_USER.name,
+							headimgurl: WX_USER.headerUrl,
+							name: formData.u_name,
+							company: formData.g_name,
+							job: formData.u_zhiwei,
+							mail: formData.u_eamil,
+							phone: formData.u_phone,
+							base64: resultBase64
+						},
+						success: function (obj) {
+							if (obj instanceof Object) {
+								//返回图片路径进入下一帧
+								var url = obj.data;
+								if (confirm("信息我们收到！报名成功将收到确认短信。")) {
+									resolve(url);
+								} else {
+									resolve(url);
+								}
+							} else { console.info("Ajax返回结果类型错误") }
+						},
+						error: function () { console.info("Ajax链接失败") }
+					});
+				});
+			});
+		}
+	})
+      //进入结果页
+	.then(function (url) {
+		fadeInResultPage(url);
+	});
+});
+
